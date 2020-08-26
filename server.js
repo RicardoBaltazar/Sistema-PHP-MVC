80de21d09543cdeb5c12f10852df2ff9ac4587df
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-26 14:28:18 -0300

    instalando novas dependencias

--- server.js
+++ server.js
@@ -15,3 +15,11 @@ app.listen(port, () => console.log(`//Server started at port${port}`))
 //use "start": "nodemon server.js", and start npm run start
 // - npm instal -D nodemon --> -D é a mesma coisa que --save-dev, instala localmente
 
+/*
+npm install --save-dev sequelize-cli
+npm install -D sequelize-cli
+
+npm install mysql2
+npm install body-parser --save
+*/
+
